dc6a934ec9f7ee7de246007cc1b9e51d0afe8cd1
Jaikangam Malangmei <jaikangam12@gmail.com>
2019-12-31 16:28:01 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Lottery from './Lottery';
 
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div className="App">
+        <Lottery /> 
+        <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
+      </div>
+     );
+  }
+}
+ 
 export default App;
